f54300a17cee8dfea63ff059b197f5f9a6a3dbc2
MrPanquecas <ricardo.simoescosta@hotmail.com>
2019-05-06 22:42:51 +0100

    server modifications

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
+import express from 'express'
+import serveStatic from 'serve-static'
+import path from 'path'
+
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
