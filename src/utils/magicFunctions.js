aa9a802dbacbc94259c242c4c49019fd9c39f627
MrPanquecas <ricardo.simoescosta@hotmail.com>
2019-05-06 23:50:41 +0100

    fix

--- src/utils/magicFunctions.js
+++ src/utils/magicFunctions.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 const arrayOfHTTP = [
   'https://www.mocky.io/v2/5cd09b0f3200000f0000fdcb',
   'https://www.mocky.io/v2/5ccb774e3300007200e017a7',
-  'http://www.mocky.io/v2/5cd0a1d73200005e0000fde5'
+  'https://www.mocky.io/v2/5cd0a1d73200005e0000fde5'
 ]
 
 export const magicFunction = () => {
